2eaab4417ef7addbf2da947062a6526d2b54ff32
Viviana Londoño <sandrislc@gmail.com>
2017-11-22 13:07:43 -0500

    graphics in report fragment

--- Proyecto/ProjectAppAccounting/app/src/main/java/com/appaccounting/compumovil/projectappaccounting/ReportFragment.java
+++ Proyecto/ProjectAppAccounting/app/src/main/java/com/appaccounting/compumovil/projectappaccounting/ReportFragment.java
@@ -12,12 +12,15 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.appaccounting.compumovil.projectappaccounting.Helpers.DbHelper;
+import com.juang.jplot.PlotBarritas;
 import com.juang.jplot.PlotPastelito;
 
 public class ReportFragment extends Fragment {
     View view;
     private PlotPastelito pastel;
+    private PlotPastelito pastel2;
     private LinearLayout pantalla;
+    private LinearLayout pantalla2;
     Context context;
     String totalGastos;
     String totalIngresos;
@@ -25,6 +28,9 @@ public class ReportFragment extends Fragment {
     float fGastos;
     float fIngresos;
     Button btnReport;
+
+    PlotBarritas Columna;
+    LinearLayout pantallaIngresos;
 
     public ReportFragment() {
         // Required empty public constructor
@@ -36,18 +42,23 @@ public class ReportFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_report, container, false);
         context = getActivity().getBaseContext();
         dbh = new DbHelper(context);
-        btnReport = (Button) view.findViewById(R.id.btnReportG);
+        btnReport = view.findViewById(R.id.btnReportG);
 
         totalGastos = dbh.countTotalDebits().toString();
         totalIngresos = dbh.countTotalEntries().toString();
         fGastos = Float.parseFloat(totalGastos);
         fIngresos = Float.parseFloat(totalIngresos);
 
-        pantalla= (LinearLayout) (view.findViewById(R.id.pantallaReport));
-        pastel=new PlotPastelito(context,"Ganancias Diarias");//puedes usar simplemente "this" en lugar de context
+        pantalla= (view.findViewById(R.id.pantallaReportGeneral));
+        pantalla2= (view.findViewById(R.id.pantallaReportGastos));
+        pantallaIngresos= (view.findViewById(R.id.pantallaReportIngresos));
+
+        pastel=new PlotPastelito(context,"Balance General");//puedes usar simplemente "this" en lugar de context
+        pastel2=new PlotPastelito(context,"Reporte Ingresos");
         float[] datapoints = {fGastos,fIngresos};
         String[] etiquetas={"Ingresos", "Gastos"};
         pastel.SetDatos(datapoints,etiquetas);
+        pastel2.SetDatos(datapoints,etiquetas);
 
    /*antes de mostrar el grafico en pantalla(LinearLayout) deben de ir todos los ajustes "Set" del grafico.
        Todos los metodos publicos que ayudan a personalizar el grafico se describen cada uno en la siguiente sección */
@@ -55,7 +66,29 @@ public class ReportFragment extends Fragment {
         pastel.SetHD(true); //ajustamos la calidad hd que suaviza bordes del grafico. por default esta desactivado
         pastel.SetColorDato(1,100,100,100);
         pastel.SetColorDato(2,200,100,100);
+        pastel.SetColorFondo(0,0,0);
+
+        pastel2.SetHD(true); //ajustamos la calidad hd que suaviza bordes del grafico. por default esta desactivado
+        pastel2.SetColorDato(1,300,300,300);
+        pastel2.SetColorDato(2,250,0,0);
+        pastel2.SetColorFondo(255,255,255);
         pantalla.addView(pastel);
+        pantalla2.addView(pastel2);
+
+
+        //Reporte ingresos
+        String x[]={"Saldo","Ingresos","Gastos","jueves","viernes"};
+        double y[]={20,30,44,0,-25};
+        Columna=new PlotBarritas(context,"Gráfico de Columnas ","Monto");
+        //en context puede colocarse simplemente this
+        //personalizacion del grafico
+        Columna.Columna(x,y);// OJO x e y DEBEN SER DEL MISMO TAMAÑO O CAUSARA QUE SE CIERRE LA APLICACION.
+        Columna.SetHD(true);
+        //cambiemos el color del dato 3 o sea "44" rojo=255,verde=0,Azul=0 los ultimos tres enteros son los colores en rgb
+        Columna.SetColorPila(2 ,200,0,120);//muestra el tercer dato en color rojo
+        //mostrando en pantalla
+        pantallaIngresos.removeAllViews();
+        pantallaIngresos.addView(Columna);
 
 
         btnReport.setOnClickListener(new View.OnClickListener()
@@ -70,6 +103,10 @@ public class ReportFragment extends Fragment {
         return view;
     }
 
+    /**
+     * Método para el botón
+     * @param v
+     */
     private void verReporteGeneral(View v){
         Intent repActivity = new Intent(context, ReportGeneralActivity.class);
         startActivity(repActivity);
